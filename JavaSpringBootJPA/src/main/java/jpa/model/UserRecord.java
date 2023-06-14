f3ee02b8781c1e3fd439c8c315147b96eac73ecd
amir-memarian <memarian.email@gmail.com>
2023-06-14 21:52:03 +0330

    init 01

---
+++ JavaSpringBootJPA/src/main/java/jpa/model/UserRecord.java
@@ -0,0 +1,45 @@
+package jpa.model;
+
+import jakarta.persistence.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class UserRecord {
+
+    @Id
+    private int id;
+    private String name;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
--- JavaSpringBootJPA/src/test/java/jpa/JavaSpringBootJpaApplicationTests.java
+++ JavaSpringBootJPA/src/test/java/jpa/JavaSpringBootJpaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.javaspringbootjpa;
+package jpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
